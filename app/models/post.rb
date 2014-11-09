e405d4276344fc2a1b409e9d78ae86dea2d6512d
伊藤総一郎 <itosouplus@itousouichirous-MacBook-Air.local>
2014-11-09 09:48:54 +0900

    done merge

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,4 @@
 class Post < ActiveRecord::Base
-<<<<<<< HEAD
 	has_many :likes
-=======
-  mount_uploader :image, ImageUploader
->>>>>>> 8f79a8781e5cef71861da1d18014ebb8947f8bbf
+	# mount_uploader :image, ImageUploader
 end
